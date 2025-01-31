7dcc4feb8bb335eb10d199f96d0e8d0351a4336d
Aman Verma <ammy@Amans-MacBook-Air.local>
2025-01-31 14:55:33 +0530

    Fix import issues

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// src/config.ts
+export const BACKEND_URL = `${import.meta.env.VITE_BACKEND_URL}` as string; // Replace with your actual backend URL
+
+export const CLOUDINARY_URL = `${
+  import.meta.env.VITE_CLOUDINARY_URL
+}` as string;
